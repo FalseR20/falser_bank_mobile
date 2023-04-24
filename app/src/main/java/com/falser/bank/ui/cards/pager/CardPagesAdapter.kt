e5c763b0d4cd60bfd4f8c9e913d8528b9de9a8d8
FalseR20 <kno3.2002@gmail.com>
2023-04-24 13:06:05 +0300

    feat: add usage new card page

--- app/src/main/java/com/falser/bank/ui/cards/pager/CardPagesAdapter.kt
+++ app/src/main/java/com/falser/bank/ui/cards/pager/CardPagesAdapter.kt
@@ -6,11 +6,16 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class CardPagesAdapter(fragmentActivity: FragmentActivity) :
     FragmentStateAdapter(fragmentActivity) {
+
+    private val length = 2
+    private var fragments: Array<CardPageFragment> =
+        Array(length) { i: Int -> CardPageFragment(i == length - 1) }
+
     override fun getItemCount(): Int {
-        return 1
+        return length
     }
 
     override fun createFragment(position: Int): Fragment {
-        return CardPageFragment()
+        return fragments[position]
     }
 }
